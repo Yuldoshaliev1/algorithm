7c17b7ad277cfc6f63462f66ca4b8b0f32af3323
Yuldoshaliyev1 <akmalyuldoshaliev@gmail.com>
2023-01-06 23:52:31 +0500

    add files

---
+++ Leetcode/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,10 @@
+class Solution:  # Time: O(NLogN) and Space: O(1)
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1 = nums1 + nums2
+        nums1 = sorted(nums1)
+        n = len(nums1)
+        if n % 2 == 0:
+            return (nums1[n//2 - 1] + nums1[(n//2)])/2
+        else:
+            n = math.ceil(n/2)
+            return nums1[n-1]
